f756fb5e43a44560a8045974cf05016e9ee70a1c
maximrock <maxrockvardill@gmail.com>
2026-01-14 20:37:59 +0300

    Adding ansible role user-roles-config, umask, config-nginx

--- shared-dir/scripts/60-config-umask.sh
+++ shared-dir/scripts/60-config-umask.sh
@@ -10,6 +10,19 @@ set -e
 config_profile_d="${config_profile_d:-/etc/profile.d}"
 umask_file="$config_profile_d/10-umask-profile.sh"
 
+umask_test_directory="${umask_test_directory:-/umask-test}"
+umask_file_test="$umask_test_directory/umask-test-file.test"
+
+umask 027
+
+mkdir -p "$umask_test_directory"
+chown -R vagrant:vagrant "$umask_test_directory"
+
+touch "$umask_test_directory/umask-test-file.test"
+chown vagrant:vagrant "$umask_test_directory/umask-test-file.test"
+
+echo "$(ls -la $umask_test_directory && umask)"
+
 if [ ! -f "$umask_file" ]; then
     sudo tee "$umask_file" > /dev/null << 'EOF'
 # Set default umask for all users
